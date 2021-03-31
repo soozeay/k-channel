c84bee99a9700c2401b365b36dc3a28da931ad65
soozeay <80019801+soozeay@users.noreply.github.com>
2021-03-31 20:30:51 +0900

    Userモデルバリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
+  validates :password, :password_confirmation, format: { with: /\A(?=.*?[a-z])(?=.*?[\d)])[a-z\d]+\z/i }
+  validates :age, numericality: { greater_than_or_equal_to: 20, less_than_or_equal_to: 120 }
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  with_options presence: true, numericality: { other_than: 0 } do
+    validates :gender_id
+    validates :country_id
+  end
+
+  belongs_to :gender
+  belongs_to :country
 end
