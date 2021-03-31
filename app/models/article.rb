5d72e5d48d1ab8e17b1787ebdad2c6443c6e700a
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-01 08:53:54 +0900

    モデルファイルの記述

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,13 @@
 class Article < ApplicationRecord
+  with_options presence: true do
+    validates :title
+    validates :text
+    validates :ingredients
+    validates :trick
+  end
+  belongs_to :user
+  belongs_to :plaza
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  validates :plaza_id, presence: true, numericality: { other_than: 0 }
+  
 end
