e35899b29810eecd3e78cb9a4e4d1ca9f4ad7ee6
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-01 09:23:26 +0900

    コントローラーの記述

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,5 +4,20 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if article.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def article_params
+    params.require(:articles).permit(:title, :text, :ingresients, :trick, :plaza_id, :image).merge(user_id: current_user.id)
   end
 end
