ba728a0fb97188323e6af8590dba06bf76d90f1a
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-01 09:03:58 +0900

    モデルファイルの修正
    Add: ActiveHash

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,6 +7,8 @@ class Article < ApplicationRecord
   end
   belongs_to :user
   belongs_to :plaza
+  has_one_attached :image
+  
   extend ActiveHash::Associations::ActiveRecordExtensions
   validates :plaza_id, presence: true, numericality: { other_than: 0 }
   
