54719e4cd6bd839dd36778c3651180dccc16b86c
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-02 22:22:56 +0900

    カラム名の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   end
   
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :age, :gender_id, :country_id, :text])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :text, :cover, :avater, :text])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :age, :gender_id, :country_id, :intro])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :intro, :cover, :avater,])
   end
 end
