7150c9076a51d347eeaccb4f4688edef43bfd329
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-03 23:48:31 +0900

    コントローラーの記述

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,18 @@
 class CommentsController < ApplicationController
 
   def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to article_path(@comment.article)
+    else
+      @article = @comment.article
+      @comments = @article.comments
+      render "/articles/show"
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:comment).merge(user_id: current_user.id, article_id: params[:article_id])
   end
 end
