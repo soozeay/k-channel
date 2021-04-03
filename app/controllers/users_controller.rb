14673999296dc1e6866868f20307ef709fb6f26d
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-03 09:29:51 +0900

    authenticate_user!の修正
    各コントローラー内でのアクションを限定して再度付与

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, except: :show
   before_action :set_user, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
 
 
   def show
@@ -24,5 +26,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:nickname, :email, :password, :password_confirmation, :cover, :avater, :intro)
+  end
+
+  def move_to_index
+    redirect_to root_path if @user.id != current_user.id
   end
 end
