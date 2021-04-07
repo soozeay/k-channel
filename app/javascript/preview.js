9f3cc665eafc68c7ddec445b970e48ca2b0458b8
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-08 08:35:29 +0900

    取得した画像情報のURLを生成

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   const ImagePreview = document.getElementById('image-preview');
   document.getElementById('article-image').addEventListener('change', function(e){
-    console.log(123)
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
   });
 });
