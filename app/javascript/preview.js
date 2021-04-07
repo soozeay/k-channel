a3464b3d585e3f9fa31aa6091985a6b63de33c38
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-08 08:59:26 +0900

    生成HTMLにクラスを付与し、リファクタリング

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,6 +1,16 @@
 if(document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
   document.addEventListener('DOMContentLoaded', function() {
     const ImagePreview = document.getElementById('image-preview');
+
+    const createImageHTML = (blob) =>{
+      const imageElement = document.createElement('div');
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('src', blob);
+      blobImage.setAttribute('class', 'preview-image');
+      imageElement.appendChild(blobImage);
+      ImagePreview.appendChild(imageElement);
+    };
+
     document.getElementById('article-image').addEventListener('change', function(e){
       const imageContent = document.querySelector('img');
       if(imageContent){
@@ -10,12 +20,7 @@ if(document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
       const file = e.target.files[0];
       const blob = window.URL.createObjectURL(file);
 
-      const imageElement = document.createElement('div');
-      const blobImage = document.createElement('img');
-      blobImage.setAttribute('src', blob);
-
-      imageElement.appendChild(blobImage);
-      ImagePreview.appendChild(imageElement);
+      createImageHTML(blob);
     });
   });
 }
