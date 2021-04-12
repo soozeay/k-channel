35580ac2e6169a4094f5c92ff2274d7970e917cb
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-12 16:08:48 +0900

    モデルファイルの記載ミスを修正

--- app/models/article_tag_relation.rb
+++ app/models/article_tag_relation.rb
@@ -1,19 +1,4 @@
 class ArticleTagRelation < ApplicationRecord
-  include ActiveModel::Model
-  attr_accessor :title, :ingredients, :text, :trick, :name
-
-  with_options presence: true do
-    validates :title
-    validates :text
-    validates :ingredients
-    validates :trick
-    validates :name
-  end
-
-  def save
-    article = Article.create(title: title, ingredients: ingredients, text: text, trick: trick, youtube_url: youtube_url)
-    tag = Tag.create(name: name)
-
-    ArticleTagRelation.create(article_id: article.id, tag_id: tag.id)
-  end
+  belongs_to :article
+  belongs_to :tag
 end
