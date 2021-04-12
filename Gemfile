0d14c59a4a10eefd92c30e6500b527a801334abf
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-13 08:58:04 +0900

    DBをリセットしGemを導入

--- Gemfile
+++ Gemfile
@@ -63,3 +63,5 @@ gem 'mini_magick'
 gem 'image_processing', '~> 1.2'
 
 gem 'pry-rails'
+
+gem 'acts-as-taggable-on', '~> 7.0'
--- db/migrate/20210412061901_create_article_tag_relations.rb
+++
@@ -1,9 +0,0 @@
-class CreateArticleTagRelations < ActiveRecord::Migration[6.0]
-  def change
-    create_table :article_tag_relations do |t|
-      t.references :article, foreign_key: true
-      t.references :tag, foreign_key: true
-      t.timestamps
-    end
-  end
-end
