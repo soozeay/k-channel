2a1552a5e7f6405267fb67eea8b1d9ae9ddc3b16
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-13 16:41:09 +0900

    ransackの処理をApplicatinControllerに記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_q_for_article
 
   private
   def basic_auth
@@ -13,5 +14,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :age, :gender_id, :country_id, :intro])
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :intro, :cover, :avater,])
+  end
+
+  def set_q_for_article
+    @q = Article.ransack(params[:q])
   end
 end
