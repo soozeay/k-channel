42415949453efbb55391e7089c0d67fbdcd01324
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-13 16:42:15 +0900

    過去の検索処理記述を削除

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,14 +6,6 @@ class Article < ApplicationRecord
   has_rich_text :text
   acts_as_taggable
 
-  def self.search(search)
-    if search != ""
-      Article.where(['title LIKE(?) OR ingredients LIKE(?) OR trick LIKE(?)', "%#{search}%", "%#{search}%", "%#{search}%"])
-    else
-      Article.all
-    end
-  end
-
   extend ActiveHash::Associations::ActiveRecordExtensions
   validates :plaza_id, presence: true, numericality: { other_than: 0 }
   belongs_to :plaza
