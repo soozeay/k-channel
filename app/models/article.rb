19f896b35dc7b549480743597ce5e5fef67b22c2
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-14 08:44:28 +0900

    articleモデルへアソシエーション記述

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,8 @@ class Article < ApplicationRecord
   
   belongs_to :user
   has_many :comments
+  has_many :likes
+  has_many :liked_users, through: :likes, source: :user
   has_one_attached :image
   has_rich_text :text
   acts_as_taggable
