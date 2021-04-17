d60d529a8b96c18dc1bfde2fda60ba39d1d1e4d8
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-17 14:22:13 +0900

    tagsinput導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 require("../preview")
 require("../tag")
 require("jquery")
+require('tagsinput')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
