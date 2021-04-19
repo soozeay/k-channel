76250c1897687f1d231116069da8032a7c24bc32
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-19 16:17:55 +0900

    bootstrapの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,8 @@ require("../preview")
 require("../tag")
 require("jquery")
 require('tagsinput')
-
+import 'bootstrap';
+import '../stylesheets/application';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
