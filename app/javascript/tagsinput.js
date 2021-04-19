cc94cb17768940e481b38ce6f41559a8a77d2a82
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-19 16:18:19 +0900

    tagsinputを導入

--- app/javascript/tagsinput.js
+++ app/javascript/tagsinput.js
@@ -22,7 +22,7 @@
     },
     freeInput: true,
     addOnBlur: true,
-    maxTags: undefined,
+    maxTags: 5,
     maxChars: undefined,
     confirmKeys: [13, 44],
     delimiter: ',',
@@ -53,7 +53,7 @@
     this.placeholderText = element.hasAttribute('placeholder') ? this.$element.attr('placeholder') : '';
     this.name = element.hasAttribute('name') ? this.$element.attr('name') : '';
     this.type = element.hasAttribute('type') ? this.$element.attr('type') : 'text';
-    this.inputSize = Math.max(1, this.placeholderText.length);
+    this.inputSize = Math.max(20, this.placeholderText.length);
 
     this.$container = $('<div class="bootstrap-tagsinput"></div>');
     this.$input = $('<input type="' + this.type + '" name="' + this.name + '" placeholder="' + this.placeholderText + '"/>').appendTo(this.$container);
