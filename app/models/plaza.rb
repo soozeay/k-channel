fe42ee0b6c769abf03f8fa2be4f79fe751ee8e9b
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-20 20:44:01 +0900

    activehashの修正

--- app/models/plaza.rb
+++ app/models/plaza.rb
@@ -1,9 +1,11 @@
 class Plaza < ActiveHash::Base
   self.data = [
     { id: 0, name: '--' },
-    { id: 1, name: 'コスメ' },
-    { id: 2, name: '語学' },
-    { id: 3, name: '料理' },
+    { id: 1, name: 'エンタメ' },
+    { id: 2, name: 'コスメ' },
+    { id: 3, name: '語学' },
+    { id: 4, name: 'グルメ情報' },
+    { id: 5, name: '日記' },
     { id: 4, name: 'その他' }
   ]
   include ActiveHash::Associations
