d5dbd9b2abb1b668ab7465d9908d133336f76ae6
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-20 20:30:57 +0900

    コントローラー記述

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,20 +1,36 @@
 class RoomsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_rooms, only: [:index, :show]
+
+  def index
+  end
+
   def create
     @room = Room.create
-    @entry1 = Entry.create(:room_id => @room.id, :user_id => current_user.id)
-    @entry2 = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(:room_id => @room.id))
-    redirect_to "/rooms/#{@room.id}"
+    @entry_current_user = Entry.create(room_id: @room.id, user_id: current_user.id)
+    @entry_follower = Entry.create(room_params)
+    redirect_to room_path(@room.id)
   end
 
   def show
     @room = Room.find(params[:id])
-    if Entry.where(:user_id => current_user.id, :room_id => @room.id).present?
+    if Entry.where(user_id: current_user.id, room_id: @room.id).present?
       @messages = @room.messages
       @message = Message.new
       @entries = @room.entries
+      entry = @room.entries.where.not(user_id: current_user)
+      @user = entry[0].user
     else
       redirect_back(fallback_location: root_path)
     end
+  end
+
+  private
+  def room_params
+    params.require(:entry).permit(:user_id, :room_id).merge(room_id: @room.id)
+  end
+
+  def set_rooms
+    @rooms = current_user.rooms.joins(:messages).includes(:messages).order("messages.created_at DESC")
   end
 end
