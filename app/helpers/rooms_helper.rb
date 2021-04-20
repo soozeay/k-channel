7ac4ff1a1906abe9feb95390bcbaa321eaec11c4
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-20 20:34:12 +0900

    helperの記述

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,2 +1,23 @@
 module RoomsHelper
+  def most_new_message_preview(room)
+    # 最新メッセージのデータを取得する
+    message = room.messages.order(updated_at: :desc).limit(1)
+    # 配列から取り出す
+    message = message[0]
+    # メッセージの有無を判定
+    if message.present?
+      # メッセージがあれば内容を表示
+      tag.p message.content.to_s, class: 'dm_list__content__link__box__message'
+    else
+      # メッセージがなければ[ まだメッセージはありません ]を表示
+      tag.p '[ まだメッセージはありません ]', class: 'dm_list__content__link__box__message'
+    end
+  end
+
+  def message_time(room)
+    message = room.messages.order(updated_at: :desc).limit(1)
+    message = message[0]
+    time_ago_in_words(message.created_at.to_s)
+  end
+  
 end
