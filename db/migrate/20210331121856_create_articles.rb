585c85cde864f577c95a1d2b4c41470df4630cb1
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-21 10:57:11 +0900

    articleテーブルのカラム見直し

--- db/migrate/20210331121856_create_articles.rb
+++ db/migrate/20210331121856_create_articles.rb
@@ -2,8 +2,7 @@ class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
       t.string :title,      null: false
-      t.text :ingredients,  null: false
-      t.text :trick,        null: false
+      t.text :trick
       t.integer :plaza_id,  null: false
       t.references :user,   null: false, foreign_key: true
       t.string :youtube_url 
