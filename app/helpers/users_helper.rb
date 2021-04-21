79f246f717790c78ecdbcd470cb8cdf09d6ac232
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-21 18:39:55 +0900

    ヘルパーとテンプレートを記述

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+
+  def follow_users
+    # フォローユーザーを配列で取得
+    @follow_users = Relationship.where(user_id: current_user)
+
+  end
+    
+  # @follower_users = current_user.followers
 end
