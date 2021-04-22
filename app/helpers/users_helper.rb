1e580c1cec1f154e971232d29309c619bb9fe102
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-22 11:40:24 +0900

    ヘルパーを変更

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,14 +2,14 @@ module UsersHelper
 
   def follow_users
     # フォローユーザーを配列で取得
-    @follow_users = current_user.followings
+    @follow_users = @user.followings
   end
     
   def follower_users
-    @follower_users = current_user.followers
+    @follower_users = @user.followers
   end
 
   def like_articles
-    @like_articles = current_user.likes
+    @like_articles = @user.likes
   end
 end
