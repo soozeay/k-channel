f136e2d5d5decc865dd7b77ca130b534e797853d
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-27 16:13:48 +0900

    モデルの記述ミスを訂正

--- app/models/tag.rb
+++
@@ -1,6 +0,0 @@
-class Tag < ApplicationRecord
-  has_many :article_tag_relations, dependent: :destroy
-  has_many :articless, through: :article_tag_relations
-
-  validates :name, uniqueness: true
-end
