1697191cab6c7e69096052dab58e26371bc0eb8b
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-28 20:21:10 +0900

    seeds削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-Article.create(title:'１投稿（K）', plaza_id: 1, user_id: 3)
-Article.create(title:'２投稿（K）', plaza_id: 2, user_id: 4)
-Article.create(title:'３投稿（K）', plaza_id: 3, user_id: 5)
-Article.create(title:'４投稿（K）', plaza_id: 4, user_id: 6)
-Article.create(title:'５投稿', plaza_id: 5, user_id: 7)
-Article.create(title:'６投稿', plaza_id: 6, user_id: 8)
-Article.create(title:'７投稿', plaza_id: 1, user_id: 9)
-Article.create(title:'８投稿', plaza_id: 2, user_id: 10)
