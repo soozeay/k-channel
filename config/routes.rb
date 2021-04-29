83e39a2fab92dfb3f0033673f433d1da827fb156
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-29 12:00:44 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
     registrations: "users/registrations",
     sessions: 'users/sessions'
   }
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
 
   root "articles#index"
   resources :articles do
