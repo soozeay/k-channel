e5b9496e03ba289c199d1f383d504ab761b4ca00
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-29 12:12:33 +0900

    ゲストユーザー削除の無効化

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_account_update_params, only: [:update]
+  before_action :ensure_normal_user, only: :destroy
 
   protected
   def update_resource(resource, params)
@@ -12,5 +13,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+  end
+
+  def ensure_normal_user
+    if resource.email == 'guest@example.com'
+      redirect_to root_path, alert: 'ゲストユーザーは削除できません。'
+    end
   end
 end
