b36b250d8c2a5bf01b621a90de8371a842462d9f
soozeay <80019801+soozeay@users.noreply.github.com>
2021-05-02 23:37:50 +0900

    カラムをmessageに変更

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -7,7 +7,7 @@ module RoomsHelper
     # メッセージの有無を判定
     if message.present?
       # メッセージがあれば内容を表示
-      tag.p message.content.to_s, class: 'dm_list__content__link__box__message'
+      tag.p message.message.to_s, class: 'dm_list__content__link__box__message'
     else
       # メッセージがなければ[ まだメッセージはありません ]を表示
       tag.p '[ まだメッセージはありません ]', class: 'dm_list__content__link__box__message'
