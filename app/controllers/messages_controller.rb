82aebd4b54e6eddc836ea89300d1348cbb24edfd
soozeay <80019801+soozeay@users.noreply.github.com>
2021-05-02 23:39:28 +0900

    コントローラーにActionCableに対応した記述を追記

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,12 @@
 class MessagesController < ApplicationController
-  before_action :authenticate_user!, :only => [:create]
+  before_action :authenticate_user!, only: :create
 
   def create
     if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
-      @message = Message.create(params.require(:message).permit(:user_id, :content, :room_id).merge(user_id: current_user.id))
-      redirect_to  room_path(@message.room_id)    # "/rooms/#{@message.room_id}"
+      @message = Message.create(params.require(:message).permit(:user_id, :message, :room_id).merge(user_id: current_user.id))
+      @room = @message.room
+      redirect_to  room_path(@message.room_id)
+      ActionCable.server.broadcast 'room_channel', message: @message, room_id: @room.id, user_image: current_user.avatar
     else
       redirect_back(fallback_location: root_path)
     end
