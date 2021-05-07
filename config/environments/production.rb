2cfdcb5ca07ddbcc4f77647c930d0220c21fe131
soozeay <80019801+soozeay@users.noreply.github.com>
2021-05-07 20:16:35 +0900

    action cableを本番環境で動作させる設定を修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,4 +111,8 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   config.assets.initialize_on_precompile = false
+
+  ActionCable.server.config.disable_request_forgery_protection = true
+  config.action_cable.url = 'ws://Elastic IP/cable'
+  config.action_cable.allowed_request_origins = [ 'http://Elastic IP' ]   
 end
