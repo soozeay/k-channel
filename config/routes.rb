3edcb6a279057020915b29cb4ce18b52e9dda9d1
soozeay <80019801+soozeay@users.noreply.github.com>
2021-05-07 09:33:28 +0900

    routesファイルの並び替え見直し

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registrations: "users/registrations",
-    sessions: 'users/sessions'
+    registrations: "users/registrations"
   }
-
-  # ゲストログイン機能
-  devise_scope :user do
-    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
-  end
 
   # ActionCable
   mount ActionCable.server => '/cable'
@@ -22,4 +16,9 @@ Rails.application.routes.draw do
   resources :notifications, only: :index
   resources :messages, only: :create
   resources :rooms, only: [:create, :show, :index]
+
+  # ゲストログイン機能
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
 end
