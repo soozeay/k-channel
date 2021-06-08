220b5915f548b5cbbf8574153dd0b7c01a9ede23
soozeay <80019801+soozeay@users.noreply.github.com>
2021-06-08 19:30:17 +0900

    各ルーティングをscopeで囲う

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,35 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    registrations: "users/registrations",
-    sessions: "users/sessions"
-  }
+  scope "(:locale)" do
+    devise_for :users, controllers: {
+      registrations: "users/registrations",
+      sessions: "users/sessions"
+    }
 
-  # ActionCable
-  mount ActionCable.server => '/cable'
+    # ActionCable
+    mount ActionCable.server => '/cable'
 
-  root "articles#index"
-  resources :articles do
-    resources :comments, only: :create
-    resources :likes, only: [:create, :destroy]
-  end
-  resources :users, only: [:show, :edit, :update]
-  resources :relationships, only: [:create, :destroy]
-  resources :notifications, only: :index
-  resources :messages, only: [:create, :destroy] do
-    member do
-      get 'correction'
-      post 'correct'
+    root "articles#index"
+
+  
+    resources :articles do
+      resources :comments, only: :create
+      resources :likes, only: [:create, :destroy]
     end
-  end
-  resources :rooms, only: [:create, :show, :index]
 
-  # ゲストログイン機能
-  devise_scope :user do
-    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+    resources :users, only: [:show, :edit, :update]
+    resources :relationships, only: [:create, :destroy]
+    resources :notifications, only: :index
+    resources :messages, only: [:create, :destroy] do
+      member do
+        get 'correction'
+        post 'correct'
+      end
+    end
+    resources :rooms, only: [:create, :show, :index]
+
+    # ゲストログイン機能
+    devise_scope :user do
+      post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+    end
   end
 end
