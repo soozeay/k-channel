d4b0a2324586925a666d62e61df6eb783b8fecb3
soozeay <80019801+soozeay@users.noreply.github.com>
2021-06-14 17:50:17 +0900

    いいねの多い順にデータを取得させる
    ※国別のボタンをクリックした場合は国別のランキングが表示される

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,11 @@ class ArticlesController < ApplicationController
         @articles = Article.where(user_id: @users.ids, plaza_id: params[:plaza_id]).order('created_at DESC')
       elsif params[:plaza_id].present? # プラザのみ選択した場合
         @articles = Article.where(plaza_id: params[:plaza_id]).includes(:user).order('created_at DESC')
-      elsif params[:like].present? # いいね人気順表示を選択した場合
+      elsif params[:country_id].present? && params[:like].present? # いいね人気順表示、且つ地域別に切り替える
+        @users = User.where(country_id: params[:country_id])
+        @articles = Article.where(id: Like.group(:article_id).order('count(article_id) DESC').pluck(:article_id))
+        @articles = @articles.where(user_id: @users.ids).limit(10)
+      elsif params[:like].present? # いいね人気順表示のみ
         @articles = Article.where(id: Like.group(:article_id).order('count(article_id) DESC').limit(10).pluck(:article_id))
       else # 地域別で全ての投稿を表示したい場合
         @users = User.where(country_id: params[:country_id])
