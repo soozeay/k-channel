4f62a89199e493acb5678e55b3fc4013c170cfa2
soozeay <80019801+soozeay@users.noreply.github.com>
2021-06-15 21:41:14 +0900

    開発環境下でのメール送信を記述

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,9 @@
 class ContactMailer < ApplicationMailer
+  default from: "example@example.com"
+
   def contact_mail(contact, user)
     @contact = contact
-    mail to: user.email, subject: "Kchannelへのお問い合わせについて【自動送信メール】"
+    mail to: user.email, bcc: ENV["ACTION_MAILER_USER"], subject: "Kchannelへのお問い合わせについて【自動送信メール】"
+    binding.pry
   end
 end
