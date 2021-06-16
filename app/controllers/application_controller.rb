aa1388c6c30937ed9a73c6fa146ad3e64f412c47
soozeay <80019801+soozeay@users.noreply.github.com>
2021-06-16 11:20:06 +0900

    不要な記述を削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,9 +21,5 @@ class ApplicationController < ActionController::Base
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
-
-  def default_url_options(options = {})
-    { locale: I18n.locale }.merge options
-  end
   
 end
