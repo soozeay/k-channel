9b3af23e57d5bba71ffe003459dad4a2a93b90a5
soozeay <80019801+soozeay@users.noreply.github.com>
2021-06-27 23:45:15 +0900

    コントローラー記述

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,2 +1,16 @@
 class NewsController < ApplicationController
+
+  def index
+    require 'news-api'
+    news = News.new(ENV['NEWS_API_KEY'])
+    if I18n.locale.to_s == "ja"
+      @news = news.get_top_headlines(country: 'jp')
+    else
+      @news = news.get_top_headlines(country: 'kr')
+    end
+    if params[:category].present?
+      @news = news.get_top_headlines(country: params[:country], category: params[:category])
+    end
+  end
+
 end
